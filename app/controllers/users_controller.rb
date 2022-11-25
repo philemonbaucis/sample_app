df1142240d31c92ad121e028c77e321473e041b3
eugenio <c3po@c3po>
2022-11-25 19:36:19 -0300

    7.2 completo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationControlle
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
+    @user = User.new
   end
 end
